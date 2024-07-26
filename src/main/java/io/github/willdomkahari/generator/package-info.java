39182511056250cfe8136dc958d244ee4975fa0c
willdomkahari <drclae@gmail.com>
2024-07-26 12:15:18 +0200

    Completed javadocs documentation

---
+++ src/main/java/io/github/willdomkahari/generator/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Contains API methods to generate random characters from a defined ruleset.
+ * @author <a href="mailto:developer.wadu@gmail.com">Willdom Kahari</a>
+ */
+package io.github.willdomkahari.generator;
